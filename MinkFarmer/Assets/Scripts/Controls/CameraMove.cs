7724bbfc4b897fd3ff602a6a0254fa26b09f8c72
patrick123900 <p.patrickkirk@gmail.com>
2020-11-06 02:47:57 +0100

    Cam og tiling

---
+++ MinkFarmer/Assets/Scripts/Controls/CameraMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float zoomSpeed = 1;
+    public float minimumZoom = 1;
+    public float maximumZoom = 5;
+
+    private Vector3 Origin;
+    private Vector3 Diference;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Origin = MousePos();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Diference = MousePos() - transform.position;
+            transform.position = Origin - Diference;
+        }
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - Input.mouseScrollDelta.y * zoomSpeed, minimumZoom, maximumZoom);
+    }
+
+    Vector3 MousePos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ MinkFarmer/Assets/Scripts/Systems/Tiling/TileBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBuilder : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int xSize = 32;
+    public int ySize = 32;
+
+    private void Start()
+    {
+        for (int i = 1; i < xSize; i++)
+        {
+            for (int j = 1; j < ySize; j++)
+            {
+                GameObject go = Instantiate(tilePrefab, new Vector3(i * 0.5f, j * 0.5f, 1), Quaternion.identity);
+                go.transform.parent = transform;
+            }
+        }
+    }
+}
